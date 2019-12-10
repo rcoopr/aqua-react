16d1b5aaf75c5c4670a92cf9a5e9c1006904a111
Froskk <37559715+Froskk@users.noreply.github.com>
2019-12-10 22:58:52 +0000

    commented unused method

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -2,10 +2,10 @@ import React from "react";
 import Tile from "../tile/Tile";
 import { StyledGrid } from "./GridStyle";
 
-const updateState = (oldState, index, tool) => {
-  let newState = [...oldState];
-  newState[index] = tool === "air" ? 1 : tool === "water" ? 2 : 0;
-};
+// const updateState = (oldState, index, tool) => {
+//   let newState = [...oldState];
+//   newState[index] = tool === "air" ? 1 : tool === "water" ? 2 : 0;
+// };
 
 const Grid = ({ length, boardState, setBoardState, dragging, currentTool }) => {
   return (
