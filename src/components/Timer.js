cd379f62325ef073f8620bdced163bf03af08bda
Froskk <37559715+Froskk@users.noreply.github.com>
2020-01-19 16:22:06 +0000

    Alternate timer hider

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,28 +1,47 @@
 import React, { useState } from "react";
+import Ocitcon, { Eye, EyeClosed } from "@primer/octicons-react";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleTimer } from "../redux/actions";
 import { formatTime } from "../utils/formatTime";
 import styled from "styled-components";
 
+const Clip = styled.div`
+  margin: 0.4em;
+  transition: transform 1000ms ease-in-out;
+  transform: ${props => (props.hide ? "translateY(0em)" : "translateY(-2em)")};
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+`;
+
+const Cover = styled.div`
+  min-width: 4em;
+  height: 1.4em;
+  padding: 0 0.6em;
+  cursor: pointer;
+  border-radius: 999em;
+  transition: transform 1000ms ease-in-out;
+  transform: ${props => (props.hide ? "translateY(0em)" : "translateY(2em)")};
+  background: ${props => props.theme.colors.bgLight};
+`;
+
 const Wrapper = styled.li`
   min-width: 4em;
+  padding: 0 0.6em;
   margin: 0.4em;
   display: flex;
   align-items: center;
   justify-content: center;
   position: relative;
   letter-spacing: 0.5px;
+  cursor: pointer;
+`;
 
-  &::after {
-    transition: opacity 300ms ease-in-out;
-    display: block;
-    content: "";
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    border-radius: 999em;
-    background: ${props => props.theme.colors.bgLight};
-    opacity: ${props => (props.hidden ? 1 : 0)};
+const ToggleTimerButton = styled.button`
+  border: none;
+  background: none;
+  padding: 0;
+  margin: 0.3em 0.7em 0 0;
+  & > svg {
+    width: 16px;
   }
 `;
 
@@ -40,8 +59,20 @@ export const Timer = () => {
     clearTimeout(stopwatch);
   }, 1000);
   return (
-    <Wrapper hidden={hidden} onClick={() => dispatch(toggleTimer())}>
-      <Value>{formatTime(timeSpent)}</Value>
-    </Wrapper>
+    <>
+      <Wrapper onClick={() => dispatch(toggleTimer())}>
+        <ToggleTimerButton>
+          <Ocitcon
+            icon={hidden ? EyeClosed : Eye}
+            verticalAlign="top"
+            size="small"
+          />
+        </ToggleTimerButton>
+        <Value>{formatTime(timeSpent)}</Value>
+      </Wrapper>
+      <Clip hide={hidden}>
+        <Cover hide={hidden} />
+      </Clip>
+    </>
   );
 };
