dadf72a0efff3387d2bb8aaa4bc6109a70327682
Froskk <37559715+Froskk@users.noreply.github.com>
2020-04-07 15:47:13 +0100

    feat: more boards

--- src/utils/BoardData.js
+++ src/utils/BoardData.js
@@ -21,6 +21,52 @@ const data = {
     },
     2: {
       difficulty: "easy",
+      labels: {
+        top: [4, 5, -1, 3, 2, -1],
+        left: [-1, -1, -1, 4, 5, 3]
+      },
+      regions: [
+        ["a", "a", "a", "b", "c", "c"],
+        ["d", "a", "b", "b", "b", "c"],
+        ["d", "d", "d", "b", "c", "c"],
+        ["e", "d", "b", "b", "c", "c"],
+        ["e", "e", "e", "b", "b", "c"],
+        ["f", "f", "f", "b", "b", "b"]
+      ],
+      completed: [
+        [w, w, w, a, a, a],
+        [a, w, a, a, a, a],
+        [w, w, w, a, a, a],
+        [w, w, w, w, a, a],
+        [w, w, w, w, w, a],
+        [a, a, a, w, w, w]
+      ]
+    },
+    3: {
+      difficulty: "easy",
+      labels: {
+        top: [4, 4, 3, -1, -1, 3],
+        left: [4, 1, 4, 4, 4, 1]
+      },
+      regions: [
+        ["a", "a", "b", "c", "c", "d"],
+        ["b", "b", "b", "e", "d", "d"],
+        ["b", "e", "e", "e", "d", "d"],
+        ["b", "e", "e", "f", "f", "d"],
+        ["b", "b", "e", "f", "f", "d"],
+        ["f", "f", "f", "f", "f", "d"]
+      ],
+      completed: [
+        [w, w, a, w, w, a],
+        [a, a, a, w, a, a],
+        [w, w, w, w, a, a],
+        [w, w, w, a, a, w],
+        [w, w, w, a, a, w],
+        [a, a, a, a, a, w]
+      ]
+    },
+    4: {
+      difficulty: "easy",
       size: "8x8",
       labels: {
         top: [3, 6, 5, 7, 1, 3, 3, 6],
@@ -47,7 +93,7 @@ const data = {
         [a, a, w, w, a, a, a, w]
       ]
     },
-    3: {
+    5: {
       difficulty: "hard",
       labels: {
         top: [6, 3, -1, 4, 3, 4, 3, 4],
@@ -74,7 +120,7 @@ const data = {
         [a, a, w, w, w, a, a, a]
       ]
     },
-    4: {
+    6: {
       difficulty: "hard",
       size: "8x8",
       labels: {
