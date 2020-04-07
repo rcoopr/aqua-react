3f81523249e5f12ca41e14da472028c9ba8ea930
Froskk <37559715+Froskk@users.noreply.github.com>
2020-04-07 13:10:35 +0100

    fix: completion check worth with 'air' === 'empty'

--- src/utils/arraysEqual.js
+++ src/utils/arraysEqual.js
@@ -1,5 +1,8 @@
 export const arraysEqual = (a, b) => {
-  const equalsB = (element, index) => element === b[index];
+  const equalsB = (element, index) => {
+    if (element === "AIR") element = "EMPTY";
+    return element === b[index];
+  };
   if (a.length !== b.length) return false;
   if (!a.every(equalsB)) return false;
 
